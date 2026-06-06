24796359f4c72e843d91ad56d035f78cfa36dad4
Light Play <lightplay456@gmail.com>
2026-06-06 12:50:03 +0300

    feat: задача PalindromeNumber

---
+++ Scharp/9_PalindromeNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _9_PalindromeNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindromeWithOutString(346457566));
+        Console.ReadLine();
+    }
+
+    public static bool IsPalindromeWithOutString(int x)
+    {
+        if (x < 0) 
+            return false;
+
+        int reversed = 0;
+        int a = x;
+        while (a != 0)
+        {
+            int ostatok = a % 10;
+            reversed = reversed * 10 + ostatok;
+            a /= 10;
+        }
+
+        return reversed == x;
+    }
+
+    public static bool IsPalindromeWithString(int x)
+    {
+        var stringX = x.ToString();
+        char[] digits = stringX.ToCharArray();
+
+        if (digits.SequenceEqual(digits.Reverse()))
+            return true;
+
+        return false;
+    }
+}
