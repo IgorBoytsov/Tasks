61e1239e9060219d732dedea030b0158b1cd15d5
Light Play <lightplay456@gmail.com>
2026-06-06 13:43:50 +0300

    feat: задача TwoSum2

---
+++ Scharp/167_TwoSum2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _167_TwoSum2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = TwoSum([-1, 0], -1);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                int current = numbers[left] + numbers[right];
+
+                if (current == target)
+                {
+                    return [left + 1, right + 1];
+                }
+                else if (current < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return [];
+        }
+    }
+}
